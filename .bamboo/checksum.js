c36073ae85697a0e63605b02b4978570e378054b
a.soroka <andrey.soroka@vedidev.com>
2020-05-16 11:05:11 +0300

    comparator changed,

--- .bamboo/checksum.js
+++ .bamboo/checksum.js
@@ -1,82 +1,24 @@
-const fs = require("fs");
-const crypto = require("crypto");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const rootPath = path.join(__dirname, "../");
-const oldJsonPath = path.join(rootPath, ".bamboo/old_package-lock.json");
-const jsonPath = path.join(rootPath, "package-lock.json");
-const varsPath = path.join(rootPath, ".bamboo/vars.txt");
+const check = () => {
+  const rootPath = path.join(__dirname, '../');
+  const oldJsonPath = path.join(rootPath, '.bamboo/old_package-lock.json');
+  const jsonPath = path.join(rootPath, 'package-lock.json');
+  const varsPath = path.join(rootPath, '.bamboo/vars.txt');
+  let data = new Uint8Array(Buffer.from('needRebuild=true'));
 
-let data;
-
-if (
-  fs.existsSync(oldJsonPath) === false ||
-  !fs.existsSync(path.join(rootPath, "node_modules"))
-) {
-  fs.copyFileSync(jsonPath, oldJsonPath);
-  data = new Uint8Array(Buffer.from("changed=true"));
-} else {
-  const oldJson = require(oldJsonPath);
-  const json = require(jsonPath);
-  if (JSON.stringify(oldJson) === JSON.stringify(json)) {
-    data = new Uint8Array(Buffer.from("changed=false"));
-  } else {
-    data = new Uint8Array(Buffer.from("changed=true"));
+  if (fs.existsSync(oldJsonPath) || fs.existsSync(path.join(rootPath, 'node_modules'))) {
+    const oldJson = require(oldJsonPath);
+    const json = require(jsonPath);
+    if (JSON.stringify(oldJson) === JSON.stringify(json)) {
+      data = new Uint8Array(Buffer.from('needRebuild=false'));
+      fs.writeFileSync(varsPath, data);
+      return;
+    }
   }
-}
-fs.writeFileSync(varsPath, data);
-process.exit(0);
+  fs.copyFileSync(jsonPath, oldJsonPath);
+  fs.writeFileSync(varsPath, data);
+};
 
-// const checker = (fileName) => {
-//   const filePath = path.join(__dirname, fileName);
-//   if (fs.existsSync(filePath) === false) {
-//     console.error(`File ${filePath} doesn't exist`);
-//     process.exit(1);
-//   }
-
-//   const algorithm = "all";
-
-//   let hashes = crypto.getHashes();
-//   if (algorithm !== "all") {
-//     if (hashes.indexOf(algorithm) === -1) {
-//       console.error("Unknown algorithm");
-//       process.exit(1);
-//     } else {
-//       hashes = [algorithm];
-//     }
-//   }
-//   hashes = hashes.filter((hash) => !hash.match(/^.+WithRSAEncryption$/));
-//   hashes = hashes.map(
-//     (hash) =>
-//       new Object({
-//         name: hash,
-//         object: crypto.createHash(hash),
-//       })
-//   );
-
-//   const input = fs.createReadStream(filePath);
-
-//   input.on("readable", () => {
-//     const data = input.read();
-//     if (data) {
-//       hashes.forEach((hash) => hash.object.update(data));
-//     } else {
-//       hashes.forEach((hash) =>
-//         console.log(`${hash.name} ${hash.object.digest("hex")}`)
-//       );
-//     }
-//   });
-// };
-
-// const checkSum = checker("oldChecksum.txt");
-
-// module.exports = async (Model, path, filter = {}) => {
-//   try {
-//     const items = await Model.find(filter).lean();
-//     const json = JSON.stringify(items);
-//     const fileName = `${path}/${Model.collection.name}.data.json`;
-//     fs.writeFileSync(fileName, json);
-//   } catch (err) {
-//     throw err;
-//   }
-// };
+check();
